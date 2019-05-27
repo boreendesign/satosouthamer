d0ff3999472fcea31df171fe7e0ff9acba9407f3
Stephen Morrissey <info@boreendesign.com>
2019-05-26 18:33:55 -0700

    Search to other pages

--- themes/hugo-travelify-theme/static/js/multistep-form.js
+++ themes/hugo-travelify-theme/static/js/multistep-form.js
@@ -12,7 +12,7 @@ $(".next").click(function(){
 	next_fs = $(this).parent().parent().next();
 
 	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	//$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
 	//show the next fieldset
 	next_fs.show();
@@ -23,23 +23,22 @@ $(".next").click(function(){
 			//1. scale current_fs down to 80%
 			scale = 1 - (1 - now) * 0.2;
 			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
+			//left = (now * 50)+"%";
 			//3. increase opacity of next_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({
         'transform': 'scale('+scale+')',
         'position': 'absolute'
       });
-			next_fs.css({'left': left, 'opacity': opacity});
+			//next_fs.css({'left': left, 'opacity': opacity});
+      next_fs.css({ 'opacity': opacity});
 
 		},
 		duration: 800,
 		complete: function(){
 			current_fs.hide();
 			animating = false;
-		},
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
+		}
 	});
 });
 
@@ -62,11 +61,13 @@ $(".previous").click(function(){
 			//1. scale previous_fs from 80% to 100%
 			scale = 0.8 + (1 - now) * 0.2;
 			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
+			//left = ((1-now) * 50)+"%";
 			//3. increase opacity of previous_fs to 1 as it moves in
 			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+			//current_fs.css({'left': left});
+			//previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+      previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+
 		},
 		duration: 800,
 		complete: function(){
@@ -78,8 +79,8 @@ $(".previous").click(function(){
 	});
 });
 
-$(".submit").click(function(){
-
+$(".action-button").click(function(event){
+  event.preventDefault();
 })
 
 })(jQuery);
